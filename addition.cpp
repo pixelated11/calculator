99c29e2557e4b7116015d03375e0c99b9486844b
Andrew <andaru.haryo@gmail.com>
2026-02-25 16:10:01 +0700

    Add files via upload

---
+++ addition.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+using namespace std;
+
+int main() {
+    const string programName = "Addition calculator";
+    cout << "Welcome to the program: " << programName << endl;
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    cout << "Please enter the x that will be added with y" << endl;
+    int x, y;
+    int sum;
+    cin >> x;
+    cout << "Enter the y." << endl;
+    cin >> y;
+    cout << "Calculating final results..." << endl;
+    sum = x + y;
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    cout << "Final result: " << sum << endl;
+    cout << "\nPress Enter to exit...";
+    cin.ignore();
+    cin.get();
+
+    return 0;
+}
